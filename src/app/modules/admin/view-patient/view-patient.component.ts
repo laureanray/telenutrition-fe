3a9949e38e7b9fc12909f302799954677cbf56ae
unknown <laureanraybahala@gmail.com>
2021-03-11 14:16:32 +0800

    view patient

--- src/app/modules/admin/view-patient/view-patient.component.ts
+++ src/app/modules/admin/view-patient/view-patient.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-
+import {RND} from '../../../core/models/rnd';
+import {ActivatedRoute} from '@angular/router';
+import {RndService} from '../../../core/services/rnd.service';
+import {MatDialog} from '@angular/material/dialog';
+import {ApproveRndModalComponent} from '../approve-rnd-modal/approve-rnd-modal.component';
+import { environment } from 'src/environments/environment';
+import {PatientService} from '../../../core/services/patient.service';
+import * as moment from 'moment';
+import {Patient} from '../../../core/models/patient';
 @Component({
   selector: 'app-view-patient',
   templateUrl: './view-patient.component.html',
   styleUrls: ['./view-patient.component.scss']
 })
 export class ViewPatientComponent implements OnInit {
+  patient: Patient;
+  environment: any;
+  moment: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,
+              private patientService: PatientService,
+              private dialog: MatDialog) {
+    this.moment = moment;
   }
 
+  fetchCurrentPatient(username: string): void {
+    this.patientService.getPatient(username)
+      .subscribe(res => {
+        console.log(res);
+        this.patient = res as Patient;
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  ngOnInit(): void {
+    this.environment = environment;
+    const routeParams = this.route.snapshot.paramMap;
+    const username = routeParams.get('username');
+    this.fetchCurrentPatient(username);
+  }
+  //
+  // approve(): void {
+  //   const dialogRef = this.dialog.open(ApproveRndModalComponent, {
+  //     width: '450px',
+  //     data: {
+  //       username: this.patient.username
+  //     }
+  //   });
+  //
+  //   dialogRef.afterClosed().subscribe(data => {
+  //     if (data.result === 'ok') {
+  //       this.fetchCurrentRND(this.patient.username);
+  //     }
+  //   });
+  // }
 }
