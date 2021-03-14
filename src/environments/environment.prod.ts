2b799f701d828ce80637b2dc71252007b9c17467
unknown <laureanraybahala@gmail.com>
2021-03-15 01:51:45 +0800

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://143.198.226.12/api',
-  apiRoot: 'http://143.198.226.12'
+  apiUrl: 'http://143.198.226.12:8080/api',
+  apiRoot: 'http://143.198.226.12:8080'
 };
