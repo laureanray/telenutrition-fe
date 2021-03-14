4e1fdd3b7063356dbc11e57b25691847bee7b6f2
unknown <laureanraybahala@gmail.com>
2021-03-14 22:01:42 +0800

    assign rnd works

--- src/app/modules/admin/assign-rnd-modal/assign-rnd-modal.component.ts
+++ src/app/modules/admin/assign-rnd-modal/assign-rnd-modal.component.ts
@@ -7,6 +7,7 @@ import {Observable, ReplaySubject, Subject} from 'rxjs';
 import {take, takeUntil} from 'rxjs/operators';
 import {FormControl} from '@angular/forms';
 import {MatSelect} from '@angular/material/select';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-assign-rnd-modal',
@@ -14,7 +15,7 @@ import {MatSelect} from '@angular/material/select';
   styleUrls: ['./assign-rnd-modal.component.scss']
 })
 export class AssignRndModalComponent implements OnInit, AfterViewInit, OnDestroy {
-  isApplying = false;
+  isAssigning = false;
   /** list of rnds */
   protected rnds: RND[] = [];
 
@@ -27,21 +28,29 @@ export class AssignRndModalComponent implements OnInit, AfterViewInit, OnDestroy
   /** list of rnds filtered by search keyword */
   public filteredRNDs: ReplaySubject<RND[]> = new ReplaySubject<RND[]>(1);
 
-  @ViewChild('singleSelect', { static: true }) singleSelect: MatSelect;
+  @ViewChild('singleSelect', {static: true}) singleSelect: MatSelect;
 
   /** Subject that emits when the component has been destroyed. */
-  // tslint:disable-next-line:variable-name
+    // tslint:disable-next-line:variable-name
   protected _onDestroy = new Subject<void>();
 
+  selectedRND: RND;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private dialogRef: MatDialogRef<AssignedNodesOptions>,
     private appointmentService: AppointmentService,
-    private rndService: RndService) {
+    private rndService: RndService,
+    private snackBar: MatSnackBar) {
     this.rndService.getAllRnds()
       .subscribe(rnds => {
         this.rnds = rnds as RND[];
+      });
+
+    this.rndCtrl.valueChanges
+      .subscribe(value => {
+        // console.log(value);
+        this.selectedRND = value;
       });
   }
 
@@ -70,7 +79,7 @@ export class AssignRndModalComponent implements OnInit, AfterViewInit, OnDestroy
       });
   }
 
-  protected filterRNDs(): void{
+  protected filterRNDs(): void {
     if (!this.rnds) {
       return;
     }
@@ -105,8 +114,26 @@ export class AssignRndModalComponent implements OnInit, AfterViewInit, OnDestroy
       });
   }
 
-  apply(): void {
-
+  assign(): void {
+    this.isAssigning = true;
+    this.data.appointment.rnd = {
+      id: this.selectedRND.id
+    } as RND;
+    this.data.appointment.status = 'SCHEDULED';
+    setTimeout(() => {
+      this.appointmentService.updateAppointment(this.data.appointment)
+        .subscribe(res => {
+          if (res) {
+            this.snackBar.open('Success!', undefined, {
+              duration: 3000
+            });
+          }
+        }, error => {
+          this.snackBar.open('Unexpected Error Occurred!', undefined, {
+            duration: 3000
+          });
+        });
+    });
   }
 
   onNoClick(): void {
