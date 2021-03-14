e35f1fb69797ba4ca09ccff0d91fed12179ae850
unknown <laureanraybahala@gmail.com>
2021-03-15 00:47:36 +0800

    added prod ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api',
-  apiRoot: 'http://localhost:8080'
+  apiUrl: 'http://143.198.226.12/api',
+  apiRoot: 'http://143.198.226.12'
 };
