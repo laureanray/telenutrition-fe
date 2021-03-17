608510b97928cee8de53385ff6a9c664adcdd1f4
unknown <laureanraybahala@gmail.com>
2021-03-17 17:37:35 +0800

    minor rev

--- src/app/modules/rnd/patients/patients.component.ts
+++ src/app/modules/rnd/patients/patients.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from '../../../core/authentication/authentication.service';
 
 @Component({
   selector: 'app-patients',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit(): void {
   }
