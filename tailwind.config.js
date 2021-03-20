5d86dc3ec9548aad78eda2d69055a7d8fe2cd366
unknown <laureanraybahala@gmail.com>
2021-03-20 14:43:27 +0800

    antokers

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontSize: {
+      'xs': '.85rem',
+      'sm': '.975rem',
+      'tiny': '1.05rem',
+      'base': '1.25rem',
+      'lg': '1.35rem',
+      'xl': '1.45rem',
+      '2xl': '1.6rem',
+      '3xl': '1.975rem',
+      '4xl': '2.35rem',
+      '5xl': '3.15rem',
+      '6xl': '4.25rem',
+      '7xl': '5rem',
+    }
   },
   variants: {
     extend: {},
