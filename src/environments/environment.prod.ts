3606570ddb0c27cf33301c1364c0a2835ca0f09a
unknown <laureanraybahala@gmail.com>
2021-05-05 23:32:49 +0800

    asdasd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://168.63.239.140/:8080/api',
-  apiRoot: 'http://168.63.239.140/:8080'
+  apiUrl: 'http://168.63.239.140:8080/api',
+  apiRoot: 'http://168.63.239.140:8080'
 };
